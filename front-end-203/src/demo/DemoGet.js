cd8be1f8d16569f0b063a61ad54bd9acfbf28b88
Tan Xue Wen, Rayner <raynertanxw@me.com>
2023-09-04 22:22:13 +0800

    Update DemoGet.js
    removed unused `useEffect`

--- front-end-203/src/demo/DemoGet.js
+++ front-end-203/src/demo/DemoGet.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 function DemoGet() {
     const apiUrl = process.env.REACT_APP_API_BASE_URL;
