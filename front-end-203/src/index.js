bd5cc468d5f2e8edca98fd5f94bbd2b447259a69
Ashiq <mohameda.2022@scis.smu.edu.sg>
2023-09-08 16:29:29 +0800

    Created BlueScreen component for the background

--- front-end-203/src/index.js
+++ front-end-203/src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import DemoGet from './demo/DemoGet';
+//import DemoGet from './demo/DemoGet';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <DemoGet />
+    {/* <DemoGet /> */}
     <App />
   </React.StrictMode>
 );
---
+++ front-end-203/src/login-screen/BlueScreen/BlueScreen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './BlueScreen.css';
+
+function BlueScreen() {
+  return (
+    <div className="blueBackground">
+      <h1>Your Blue Screen</h1>
+      {/* Other content */}
+    </div>
+  );
+}
+
+export default BlueScreen;
