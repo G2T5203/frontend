dcceacb566cb4859822bd746eaefc6b5d397f75e
Ashiq <mohameda.2022@scis.smu.edu.sg>
2023-09-08 16:36:19 +0800

    edited import statement as file path was wrong

--- front-end-203/src/App.js
+++ front-end-203/src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
-import BlueScreen from './login-screen/BlueScreen';
+import BlueScreen from './login-screen/BlueScreen/BlueScreen';
 
 function App() {
   return (
