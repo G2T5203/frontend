29de706c9f1b9cb3ba70745c07cf7299c68716cd
Ashiq <mohameda.2022@scis.smu.edu.sg>
2023-09-10 20:15:32 +0800

    deleted experimental features and added sign up page

--- front-end-203/src/App.js
+++ front-end-203/src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import React from 'react';
 import SignInSide from './login-screen/sign-in-side/SignInSide';
+import SignUpSide from './signup-screen/SignUpSide';
 
 
 function App() {
   return (
     <div className="App">
-      <SignInSide />
+      <SignUpSide />
     </div>
   );
 }
