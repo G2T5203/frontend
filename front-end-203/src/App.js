33e3a0f18cd24a6388a81b90f067660862660202
Ashiq <mohameda.2022@scis.smu.edu.sg>
2023-09-11 22:26:32 +0800

    cleaned up app.js and index.js for merge

--- front-end-203/src/App.js
+++ front-end-203/src/App.js
@@ -1,14 +1,26 @@
+import logo from './logo.svg';
 import './App.css';
-import React from 'react';
-import SignInPage from './signin-screen/SignInPage';
-import SignUpPage from './signup-screen/SignUpPage';
-
 
 function App() {
   return (
     <div className="App">
-      <SignInPage />
-      <SignUpPage />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+      <footer>
+        Environment {process.env.REACT_APP_ENV_NAME}
+      </footer>
     </div>
   );
 }
