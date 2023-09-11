8c988bce0f40684d00e857b942fd07d2789cb1f9
choyboys <jared.choy777@gmail.com>
2023-09-11 21:33:12 +0800

    Cleaning for merger

--- front-end-203/src/index.js
+++ front-end-203/src/index.js
@@ -2,18 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-import HomeTop from "./homepage/Top/TopArea";
-import HomeMiddle from "./homepage/MiddleAlbum/Album";
-import Footer from "./homepage/Bottom/Footer";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <HomeTop sx={{ overflow: "scrollable" }} />
-    <HomeMiddle sx={{ overflow: "scrollable" }} />
-    <Footer />
-  </React.StrictMode>
-);
+root.render(<React.StrictMode></React.StrictMode>);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
