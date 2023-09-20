62e7e896ec43d5e6f81e9bd9a75900c479e54634
QiJie53 <limqijie53@gmail.com>
2023-09-20 22:28:06 +0800

    shifted directory for cookieUtils.js

--- front-end-203/src/auth.js
+++ front-end-203/src/auth.js
@@ -21,3 +21,15 @@ export const setAuthToken = (token) => {
 //   const token = Cookies.get(TOKEN_COOKIE_NAME);
 //   return !!token;
 // };
+
+export function getCookie(cookieName) {
+  // document.cookie property to access all cookies set for the current website
+  const cookies = document.cookie.split(';').map(cookie => cookie.trim());
+  for (const cookie of cookies) {
+    const [name, value] = cookie.split('=');
+    if (name === cookieName) {
+      return decodeURIComponent(value);
+    }
+  }
+  return null; // Return null if the cookie is not found
+}
