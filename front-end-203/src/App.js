14b63bf21045b6352d08b37c8cae249030db97dc
Rayner Tan <raynertanxw@me.com>
2023-09-22 22:28:59 +0800

    Added EditPlane functionalities. Now you can edit individual planes.

--- front-end-203/src/App.js
+++ front-end-203/src/App.js
@@ -5,12 +5,15 @@ import HomePage from "./homepage/HomePage";
 import FlightSearch from "./flight-search/FlightSearch";
 import SignIn from "./signin-screen/SignInPage";
 import SignUp from "./signup-screen/SignUpPage";
+
+// TODO: This should be made common later on. But wait till after Jared does his updates and makes the nav bar component.
 import EditProfile from "./admin-portal/EditProfile";
-import PlaneAdd from "./admin-portal/AdminManagePlane";
 
 // Admin Portal
 import AdminPortalLogin from "./admin-portal/AdminPortalLogin";
 import AdminPortalHomePage from "./admin-portal/AdminPortalHomePage";
+import AdminPortalManagePlane from "./admin-portal/AdminManagePlane";
+import AdminPortalEditPlane from "./admin-portal/AdminEditPlane";
 
 function App() {
   return (
@@ -27,7 +30,8 @@ function App() {
         {/* ADMIN PORTAL */}
         <Route path="/adminPortal/home" element={<AdminPortalHomePage />} />
         <Route path="/adminPortal/login" element={<AdminPortalLogin />} />
-        <Route path="/adminPortal/planes" element={<PlaneAdd />} />
+        <Route path="/adminPortal/planes" element={<AdminPortalManagePlane />} />
+        <Route path="/adminPortal/planes/edit" element={<AdminPortalEditPlane />} />
       </Routes>
     </Router>
   );
