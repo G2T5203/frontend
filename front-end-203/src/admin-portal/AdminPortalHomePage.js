58bff0daad49b1716ad97bf9a891e7fdf1a1b500
Rayner Tan <raynertanxw@me.com>
2023-09-23 16:08:23 +0800

    UI Tweaks to admin home page.

--- front-end-203/src/admin-portal/AdminPortalHomePage.js
+++ front-end-203/src/admin-portal/AdminPortalHomePage.js
@@ -62,73 +62,77 @@ const AdminPortalHomePage = () => {
 
     return (
         <Container>
-            <Grid container
-                alignItems="center"
-                justifyContent="center"
-                style={{
-                    paddingTop: 80,
-                    padding: 20,
-                }}
-            >
-                <Grid item xs={10} md={10}>
+            <Paper sx={{
+                p: 1,
+                m: 1
+            }}>
+            <Box sx={{
+                display: 'flex',
+                justifyContent: 'space-between',
+                p: 1,
+                m: 1,
+                bgcolor: 'background.paper',
+                borderRadius: 1,
+                alignItems: 'center',
+            }}>
+                <Box>
                     <Typography variant="h3">Welcome {currentUser != null ? currentUser.username : ""}</Typography>
-                </Grid>
-                <Grid item xs={2} md={2} justify="flex end">
+                </Box>
+                <Box>
                     <Button variant="contained" color="secondary" onClick={onLogout}>Logout</Button>
-                </Grid>
-            </Grid>
+                </Box>
+            </Box>
+            </Paper>
 
 
-            <Grid container justify="center" lg={10} xl={8}>
-                <Grid item xs={6} md={6}
-                    style={{
-                        // backgroundColor: "#FF0000",
-                        padding: 10,
-                    }}
-                >
-                    <Paper elevation={3}
-                        style={{
-                            padding: 20,
-                        }}
-                    >
+            <Grid container xs={12}>
+                <Grid item xs={12} sm={8} style={{
+                    padding: 10,
+                }}>
+                    <Paper elevation={3} style={{
+                        padding: 20,
+                    }}>
                         <div>
-                            <h1>All Users</h1>
+                        <Typography variant="h4">ALL USERS</Typography>
+                        <br/>
                             {allUsers.length > 0 ? (
                                 allUsers.map(item => (
-                                    <div key={item.username}>
+                                    <Button fullWidth key={item.username} elevation={3} variant="contained"
+                                    color={item.authorityRole === "ROLE_ADMIN" ? "warning" : "primary"}
+                                    onClick={() => {console.log("TODO: navigate to edit profile page...")}}
+                                    style={{margin: 10, padding: 10, borderRadius: 8,}}>
                                         {item.salutation}. {item.firstName} {item.lastName}
                                         <br />
-                                        {item.username}
+                                        Username: {item.username}
+                                        <br />
                                         {item.email}
                                         <br />
-                                        <br />
-                                    </div>
+                                        Phone: {item.phone}
+                                    </Button>
                                 ))
                             ) : (
-                                <p>Loading... something might have went wrong (check console)</p>
+                                <Typography variant="body">Loading... something might have went wrong (check console)</Typography>
                             )}
                         </div>
                     </Paper>
-
                 </Grid>
 
-                <Grid item xs={6} md={6}
-                    style={{
-                        // backgroundColor: "#00FF00",
-                        padding: 10
-                    }}>
+                <Grid item xs={12} sm={4} style={{
+                    padding: 10
+                }}>
                     <Paper elevation={3}>
-                        <Grid container justifyContent="center">
-                            <Grid item xs={12}>
-                                <Link component={Button} href="/adminPortal/planes">Manage Planes</Link>
-                            </Grid>
-                            <Grid item xs={12}>
-                                <Link component={Button} href="/adminPortal/routes" style={{
-                                    // backgroundColor: "#FF0000"
-                                }}
-                                >Manage Routes</Link>
-                            </Grid>
-                        </Grid>
+                        <Box columnGap={2} style={{
+                            display: 'flex',
+                            flexDirection: 'column',
+                            padding: 20,
+                            alignItems: 'center',
+                        }}>
+                            <Button fullWidth variant="contained" onClick={() => { navigate("/adminPortal/planes") }}
+                            style={{margin: 10}}>Manage Planes</Button>
+
+                            <Button fullWidth variant="contained" onClick={() => { navigate("/adminPortal/routes") }}
+                            style={{margin: 10}}>Manage Routes</Button>
+                        </Box>
                     </Paper>
                 </Grid>
             </Grid>
