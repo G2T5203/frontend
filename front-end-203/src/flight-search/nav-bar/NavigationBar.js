f1d149b2c09c889599ffd62611bdd5ee71599bd3
Jared <jaredchoy2022@scis.smu.edu.sg>
2023-09-24 18:31:11 +0800

    starting nav bar

--- front-end-203/src/flight-search/nav-bar/NavigationBar.js
+++ front-end-203/src/flight-search/nav-bar/NavigationBar.js
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./NavigationBar.css"; // Import your CSS file for styling
 import NavButton from "../nav-button/NavButton"; // Import the Button component
 import ProfileIcon from "../profile-icon/ProfileIcon"; // Import the ProfileIcon component
 import LogoButton from "../logo-button/LogoButton";
+import { isAuthenticated, getCurrentUser, } from "../../auth";
+import axios from "axios";
+import { Typography } from "@mui/material";
 
 const NavigationBar = () => {
+  
+  const apiUrl = process.env.REACT_APP_API_BASE_URL;
+  let loggedin = false;
+  useEffect(() => {
+    if (isAuthenticated()) {
+      axios.get(apiUrl + "users/authTest").then((response) => {
+        if (response.status === 200) {
+          loggedin = true;
+        } else {
+          loggedin = false;
+        }
+      })
+    }
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  
+
   return (
+    loggedin ?
     <div className="navbar">
       {/* Left Logo (without hover effect) */}
       <LogoButton text={"WingIt"}></LogoButton>
@@ -16,6 +38,11 @@ const NavigationBar = () => {
 
       {/* Profile Icon */}
       <ProfileIcon />
+    </div> :
+    <div className="navbar"> 
+    <LogoButton text={"WingIt"}></LogoButton>
+
+    <Typography></Typography>
     </div>
   );
 };
