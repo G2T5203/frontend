4c5aa4e441061889934d0cca36c21a1475a5a724
Jared <jaredchoy2022@scis.smu.edu.sg>
2023-09-30 19:50:36 +0800

    made button route back to home page

--- front-end-203/src/6-confirmation/ComfirmationPage.js
+++ front-end-203/src/6-confirmation/ComfirmationPage.js
@@ -1,34 +1,57 @@
-import React from 'react';
-import BookingSlip from './BookingSlip.js';
-import { Box, Container, Typography , Button} from '@mui/material';
+import React from "react";
+import BookingSlip from "./BookingSlip.js";
+import { Box, Container, Typography, Button } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 const ConfirmationPage = () => {
-    return (<>
-        <Box sx={{
-            display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center',
-                justifyContent: 'center',
-                textAlign: 'center',        }}>
-        <Typography variant='h4' fontWeight={"bold"} sx={{textDecoration: "underline", p: 3}}>Booking Success!</Typography>
-        <Button variant={"contained"} sx={{mb:3, color: "white", backgroundColor: "#F9AB55"}}>Download Calendar File</Button>
-        </Box>
-        <Container>
-            <BookingSlip/>
-        </Container>
-            <Box sx={{
-            display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center',
-                justifyContent: 'center',
-                textAlign: 'center',        }}>
-                
-        <Button variant={"contained"} sx={{mt:3, }}>Return to home</Button>
+    const navigate = useNavigate();
 
-                </Box>
-
-        </>
-    )
-}
+    const handleClickReturn = (e) => {
+        navigate("/");
+    }
+  return (
+    <>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          textAlign: "center",
+        }}
+      >
+        <Typography
+          variant="h4"
+          fontWeight={"bold"}
+          sx={{ textDecoration: "underline", p: 3 }}
+        >
+          Booking Success!
+        </Typography>
+        <Button
+          variant={"contained"}
+          sx={{ mb: 3, color: "white", backgroundColor: "#F9AB55" }}
+        >
+          Download Calendar File
+        </Button>
+      </Box>
+      <Container>
+        <BookingSlip />
+      </Container>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          textAlign: "center",
+        }}
+      >
+        <Button variant={"contained"} sx={{ mt: 3 }} onClick={handleClickReturn}>
+          Return to home
+        </Button>
+      </Box>
+    </>
+  );
+};
 
 export default ConfirmationPage;
