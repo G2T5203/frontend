b82bb880ddf2edabdd195c1605df3b4868fea437
Ashiq <mohameda.2022@scis.smu.edu.sg>
2023-10-02 21:37:15 +0800

    added in navbar and flight banner to passenger details

--- front-end-203/src/passenger-details/PassengerDetails.js
+++ front-end-203/src/passenger-details/PassengerDetails.js
@@ -3,38 +3,31 @@ import { AppBar, Toolbar, Typography, Button, Container, TextField, Select, Menu
 import SearchIcon from '@mui/icons-material/Search';
 import ProgressBar from '../progress-bar/ProgressBar';
 import PassengerForm from './passenger-form/PassengerForm'
+import Banner from "./banner/Banner";
+import NavBar from "../nav-bar/NavigationBar"; // Import the Navbar component
+
 
 const PassengerDetails = () => {
     return (
         <div>
-            <AppBar position="static">
-                <Toolbar>
-                    <Typography variant="h6" style={{ flexGrow: 1 }}>
-                        WingIt
-                    </Typography>
-                    <Button color="inherit">Book Flights</Button>
-                    <Button color="inherit">My Bookings</Button>
-                    <SearchIcon />
-                </Toolbar>
-            </AppBar>
+            <div className="nav">
+                <NavBar />
+            </div>
+            <div className="banner">
+                <Banner />
+            </div>
             <ProgressBar />
-            <Container style={{ backgroundImage: 'url({https://media.npr.org/assets/img/2021/10/06/gettyimages-1302813215_wide-6c48e5a6aff547d2703693450c4805978de47435.jpg})', backgroundSize: 'cover', padding: '50px 0' }}>
-                <Typography variant="h5" gutterBottom>
-                    Passenger Details
-                </Typography>
-
-                <LinearProgress variant="determinate" value={50} style={{ marginBottom: '20px' }} />  {/* Adjust value as needed */}
-
-                <PassengerForm />
-
             
-                    <Button variant="contained" color="primary">
-                        Proceed to Review
-                    </Button>
+            <PassengerForm />
 
+            <div>
+                <Button variant="contained" color="primary">
+                    Proceed to Review
+                </Button>
+            </div>
 
-            </Container>
         </div>
+
     );
 }
 
