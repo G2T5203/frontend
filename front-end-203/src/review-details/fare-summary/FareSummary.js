40f1eee805cc16203adffa3ec661a7d693411c7f
Kevin Saji <kevinsaji@outlook.sg>
2023-10-21 12:21:00 +0800

    -authentication added for calculateChargedPrice api call

--- front-end-203/src/review-details/fare-summary/FareSummary.js
+++ front-end-203/src/review-details/fare-summary/FareSummary.js
@@ -1,27 +1,71 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';  
 import './FareSummary.css'
+import { useNavigate } from "react-router-dom";
+import { isAuthenticated, removeAuthToken, updateAuthHeadersFromCurrentUser } from '../../auth';
 
 const FareSummary = ({ passengers, tripType, bookingId }) => {
 
     const [totalChargedPrice, setTotalChargedPrice] = useState(0);
     const apiUrl = process.env.REACT_APP_API_BASE_URL;
+    const navigate = useNavigate();
 
-    useEffect(() => {
-        const fetchChargedPrice = async () => {
-            try {
-                const url1 = apiUrl + `bookings/calculateChargedPrice/${bookingId}`; 
-                const response = await axios.put(url1);
-                setTotalChargedPrice(response.data.totalChargedPrice);
-                console.log('Response from backend:', response.data);
-
-            } catch (error) {
-                console.error("Failed to fetch charged price:", error);
+    //authentication + api call
+  useEffect(() => {
+    if (isAuthenticated()) {
+      axios
+          .get(apiUrl + "users/authTest")
+          .then((response) => {
+            // TODO: This isn't correctly reporting errors. Postman is 403, but here it's still 200.
+            if (response.status === 200) {
+              updateAuthHeadersFromCurrentUser();
+              const fetchChargedPrice = async () => {
+                try {
+                    const url1 = apiUrl + `bookings/calculateChargedPrice/${bookingId}`; 
+                    const response = await axios.put(url1);
+                    setTotalChargedPrice(response.data.totalChargedPrice);
+                    console.log('Response from backend:', response.data);
+    
+                } catch (error) {
+                    console.error("Failed to fetch charged price:", error);
+                }
+            };
+    
+            fetchChargedPrice();
+            } else {
+              removeAuthToken();
+              navigate("/signin");
+              console.log("made an error at flightSearch 51")
             }
-        };
+          })
+          .catch((error) => {
+            removeAuthToken();
+            navigate("/signin");
+            console.log(error);
+          });
+    } else {
+      navigate("/signin");
+        console.log("made an error at flightSearch 61")
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-        fetchChargedPrice();
-    }, []);
+  // previous useeffect code for reference
+    // useEffect(() => {
+    //     const fetchChargedPrice = async () => {
+    //         try {
+    //             const url1 = apiUrl + `bookings/calculateChargedPrice/${bookingId}`; 
+    //             const response = await axios.put(url1);
+    //             setTotalChargedPrice(response.data.totalChargedPrice);
+    //             console.log('Response from backend:', response.data);
+
+    //         } catch (error) {
+    //             console.error("Failed to fetch charged price:", error);
+    //         }
+    //     };
+
+    //     fetchChargedPrice();
+    // }, []);
 
     console.log('Total Charged Price:', totalChargedPrice);
 
