301acdf5c1c1b7042a7e2d359c27848d0576959d
Ashiq <mohameda.2022@scis.smu.edu.sg>
2023-11-02 20:29:22 +0800

    homepage with images of six countries

--- front-end-203/src/homepage/MiddleAlbum/Album.js
+++ front-end-203/src/homepage/MiddleAlbum/Album.js
@@ -1,5 +1,4 @@
 import * as React from "react";
-import Button from "@mui/material/Button";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
@@ -9,9 +8,15 @@ import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 
-const cards = [1, 2, 3, 4, 5, 6];
+const destinations = [
+  { name: "Japan", price: "From $770", image: "https://rimage.gnst.jp/livejapan.com/public/article/detail/a/00/02/a0002487/img/basic/a0002487_main.jpg?20230106161700" },
+  { name: "Singapore", price: "From $504", image: "https://images.unsplash.com/photo-1565967511849-76a60a516170?auto=format&fit=crop&q=80&w=1000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8c2luZ2Fwb3JlfGVufDB8fDB8fHww" },
+  { name: "Taiwan", price: "From $606", image: "https://images.unsplash.com/photo-1621584728858-616080e4bd01?auto=format&fit=crop&q=80&w=1000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8dGFpd2FufGVufDB8fDB8fHww" },
+  { name: "India", price: "From $627", image: "https://i.natgeofe.com/k/42e832f5-fd48-43ff-b338-091bdf4048ca/india-tajmahal_16x9.jpg?w=1200" },
+  { name: "China", price: "From $858", image: "https://t3.ftcdn.net/jpg/01/00/40/82/360_F_100408242_ODbvc2HHZOC5YtSIdJrXlWxLKapI2iEG.jpg" },
+  { name: "Malaysia", price: "From $420", image: "https://www.chariotworldtours.com/wp-content/uploads/2017/08/malaysia.png" },
+];
 
-// TODO remove, this demo shouldn't need to reset the theme.
 const defaultTheme = createTheme();
 
 export default function Album() {
@@ -19,8 +24,8 @@ export default function Album() {
     <ThemeProvider theme={defaultTheme}>
       <Container sx={{ py: 8 }} maxWidth="md" disableGutters={true}>
         <Grid container spacing={4}>
-          {cards.map((card) => (
-            <Grid item key={card} xs={12} sm={6} md={4}>
+          {destinations.map((destination) => (
+            <Grid item key={destination.name} xs={12} sm={6} md={4}>
               <Card
                 sx={{
                   height: "100%",
@@ -31,24 +36,17 @@ export default function Album() {
                 <CardMedia
                   component="div"
                   sx={{
-                    // 16:9
-                    pt: "56.25%",
+                    height: 200,
+                    backgroundSize: "cover",
+                    backgroundImage: `url(${destination.image})`,
                   }}
-                  image="https://source.unsplash.com/random?wallpapers"
                 />
                 <CardContent sx={{ flexGrow: 1 }}>
-                  <Typography gutterBottom variant="h5" component="h2">
-                    Heading
+                  <Typography variant="h5" component="h2">
+                    {destination.name}
                   </Typography>
-                  <Typography>
-                    This is a media card. You can use this section to describe
-                    the content.
-                  </Typography>
+                  <Typography variant="subtitle1">{destination.price}</Typography>
                 </CardContent>
-                <CardActions>
-                  <Button size="small">View</Button>
-                  <Button size="small">Edit</Button>
-                </CardActions>
               </Card>
             </Grid>
           ))}
