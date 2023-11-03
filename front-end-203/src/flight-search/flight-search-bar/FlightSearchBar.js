386222189e1f2ba223689b0370ad00ce7ae5b3ff
Kevin Saji <kevinsaji@outlook.sg>
2023-11-03 17:41:17 +0800

    - 1 way flight search error fixed

--- front-end-203/src/flight-search/flight-search-bar/FlightSearchBar.js
+++ front-end-203/src/flight-search/flight-search-bar/FlightSearchBar.js
@@ -86,8 +86,9 @@ const FlightSearchBar = ({
   const [departureLocation, setDepartureLocation] = useState(flyingFrom ? flyingFrom : storedDepartureLocation);
   const [arrivalLocation, setArrivalLocation] = useState(flyingTo ? flyingTo : storedArrivalLocation);
   const [tripType, setTripType] = useState(trip ? trip : storedSelectedTripType);
-  const [departureDate, setDepartureDate] = useState(departuredt ? departuredt :  dayjs(storedRecentDepartureDate.replace(/"/g, "")));
-  const [returnDate, setReturnDate] = useState(returndt ? returndt : dayjs(storedRecentReturnDate.replace(/"/g, "")));
+  const [departureDate, setDepartureDate] = useState(departuredt ? departuredt :  dayjs(storedRecentDepartureDate?.replace(/"/g, "")));
+  const [returnDate, setReturnDate] = useState(returndt ? returndt : dayjs(storedRecentReturnDate?.replace(/"/g, "")));
+  
   const [passengerCount, setPassengerCount] = useState(noGuest ? noGuest : storedPax);
 
   //to set the default tripType if there is no change in toggle
